ba589ebc6860da995f97276c448af25bfd503dde
Vikram Singh Prasher <vikramsingh_prasher@student.uml.edu>
2014-08-24 15:42:11 -0400

    Hi, added the soultions to .R.

--- cachematrix.R
+++ cachematrix.R
@@ -22,3 +22,27 @@ cacheSolve <- function(x, ...) {
     x$setInverse(m)
     m
 }
+## Testing and soultions
+> a <- makeCacheMatrix(matrix(1:4,2))
+> a$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> a$set(matrix(5:8,2))
+> a$get()
+     [,1] [,2]
+[1,]    5    7
+[2,]    6    8
+> cacheSolve(a)
+     [,1] [,2]
+[1,]   -4  3.5
+[2,]    3 -2.5
+> a$getInverse()
+     [,1] [,2]
+[1,]   -4  3.5
+[2,]    3 -2.5
+> b=a$getInverse()
+> a$get() %*% b
+     [,1]         [,2]
+[1,]    1 3.552714e-15
+[2,]    0 1.000000e+00
